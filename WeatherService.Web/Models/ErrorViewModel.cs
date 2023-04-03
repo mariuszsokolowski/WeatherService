6a6a28d7e7833ef61737e1ccdf972fbecd436cde
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2023-04-03 18:32:39 +0200

    Initial commit

---
+++ WeatherService.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeatherService.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
